2df322f1e009b915c60533eabc408e0558b593d8
Emils Dzintars <edzintars@student.tudelft.nl>
2025-03-06 18:32:10 +0100

    removing

--- loggers/OutputLogger.py
+++ loggers/OutputLogger.py
@@ -45,8 +45,10 @@ def output_logger(fld):
     # UPDATED TRUSTFILE CONTENTS
     competence_search = trustfile_contents[-1]['competence_search']
     competence_rescue = trustfile_contents[-1]['competence_rescue']
+    competence_remove = trustfile_contents[-1]['competence_remove']
     willingness_search = trustfile_contents[-1]['willingness_search']
     willingness_rescue = trustfile_contents[-1]['willingness_rescue']
+    willingness_remove = trustfile_contents[-1]['willingness_remove']
     # Retrieve the number of ticks to finish the task, score, and completeness
     no_ticks = action_contents[-1]['tick_nr']
     score = action_contents[-1]['score']
@@ -60,4 +62,5 @@ def output_logger(fld):
     with open(fld + '/beliefs/allTrustBeliefs.csv', mode='a+') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         # UPDATED TRUST VALUES
-        csv_writer.writerow([name, competence_search, competence_rescue, willingness_search, willingness_rescue])
+        csv_writer.writerow([name, competence_search, competence_rescue, competence_remove,
+                             willingness_search, willingness_rescue, willingness_remove])
